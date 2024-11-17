621af615de0146bd7dfb7f7463210d69ee9c8242
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2024-11-17 15:41:04 -0700

    add initial files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rec
 Regular expression compiler
+generate a c or c++ lexer from a list of regexes
+
+work in progress
---
+++ premake5.lua
@@ -0,0 +1,17 @@
+workspace "rec"
+	configurations {"debug", "release"}
+project "rec"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	targetname "rec"
+	files {"**.cpp", "**.h"}
+	objdir "obj/%{cfg.buildcfg}"
+	targetdir "bin/%{cfg.buildcfg}"
+	filter "configurations:debug"
+		defines { "DEBUG" }
+		symbols "On"
+		optimize "Debug"
+	filter "configurations:release"
+		defines { "NDEBUG" }
+		optimize "On"
