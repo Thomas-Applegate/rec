853cc4c70d33e53135df28918b00985958984b6a
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2024-11-17 16:10:47 -0700

    created named struct

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # rec
-Regular expression compiler
-generate a c or c++ lexer from a list of regexes
+Regular expression compiler.
 
-work in progress
+generate a c or c++ lexer from a list of regexes.
+
+work in progress.
--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+
+#include "named.h"
+
+std::vector<named<std::string>> read_input(std::istream& is)
+{
+	std::vector<named<std::string>> ret;
+	return ret;
+}
 
 int main(int argc, const char** argv)
 {
+	if(argc >= 2)
+	{
+		
+	}else
+	{
+		
+	}
+	
 	return 0;
 }
