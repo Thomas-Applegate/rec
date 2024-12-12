4e43036d2404a5dcf5e19911dc3b3eaf89b9eca2
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2024-12-12 10:38:10 -0700

    remove named struct

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@
 #include <string>
 #include <vector>
 
-#include "named.h"
-
-std::vector<named<std::string>> read_input(std::istream& is)
-{
-	std::vector<named<std::string>> ret;
-	return ret;
-}
-
 int main(int argc, const char** argv)
 {
 	if(argc >= 2)
