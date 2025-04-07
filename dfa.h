d035af8bce8e06033d253ba2999e24258310108d
Thomas Applegate <78666284+Thomas-Applegate@users.noreply.github.com>
2025-04-07 11:48:32 -0600

    remove old regex parsing code

--- dfa.h
+++ dfa.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <string>
+#include <string_view>
 #include <exception>
 #include <variant>
 #include <map>
@@ -33,8 +33,7 @@ public:
 		std::set<size_t> omega_transitions; //any character transition
 	};
 
-	Nfa(const std::string& regex);
-	Nfa(const char* regex);
+	Nfa(std::string_view regex);
 	
 	operator const std::vector<state>&() const noexcept;
 	const std::vector<state>& states() const noexcept;
@@ -45,13 +44,17 @@ private:
 	size_t emplace_new_state();
 	//recursively parses a regex, takes the string and input state
 	//returns the index of the final state of the regex
-	size_t parse_regex(const char*& str, size_t in_state);
+	
+	//TODO size_t parse_regex(const char*& str, size_t in_state);
+	
 	//recursively parses a chunk of a regex, takes the string and input state
 	//returns the index of the final state of the chunk
-	size_t parse_chunk(const char*& str, size_t in_state);
+	
+	//size_t parse_chunk(const char*& str, size_t in_state);
+	
 	//recursively parses a single element of a regex, takes the string and input state
 	//returns the index of the final state of the chunk
-	size_t parse_element(const char*& str, size_t in_state);
+	//TODO size_t parse_element(const char*& str, size_t in_state);
 };
 
 class Dfa
